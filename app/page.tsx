0a7b8607942e2b38c87e0cff91b0c5b1fa45ac07
shandohowell <shandohowell94@gmail.com>
2026-05-09 19:13:47 -0500

    Working on home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,24 +8,31 @@ export default async function Home() {
 
   return (
     <div>
-      <div className="sm:py-2 py-1 sm:text-6xl text-4xl text-foreground items-center text-center sm:m-16 m-12">
-        <h1>Gain SEO Clarity</h1>
-      </div>
-      <div className="items-center text-center sm:py-4 py-2">
-        {userId ? 
-          <Link href="/dashboard" className="bg-foreground p-4 text-background">
-            Generate A Report
-          </Link>
-        : <SignInButton
-            mode="modal"
-            fallbackRedirectUrl={"/dashboard"}
-            forceRedirectUrl={"/dashboard"}
-          >
-              <button className="bg-foreground p-4 text-background">
-                Sign In
-              </button>
-          </SignInButton>
-        }
+      {/* Header section */}
+      <div className="py-24 text-foreground items-center text-center">
+        <h1 className="text-6xl py-4">Gain clarity in your keyword strategy.</h1>
+        <h2 className="text-4xl py-2">Know what is working for you.</h2>
+        <div className="py-6 px-60">
+          <p className="text-2xl">
+            Generate comprehensive SEO reports with accurate data that you can use to level up your online presence.
+          </p>
+        </div>
+        <div className="items-center text-center py-12">
+          {userId ? 
+            <Link href="/dashboard" className="bg-foreground px-24 py-4 text-background">
+              Generate A Report
+            </Link>
+          : <SignInButton
+              mode="modal"
+              fallbackRedirectUrl={"/dashboard"}
+              forceRedirectUrl={"/dashboard"}
+            >
+                <button className="bg-foreground px-24 py-4 text-background">
+                  Sign In
+                </button>
+            </SignInButton>
+          }
+        </div>
       </div>
     </div>
   ) 
